c0ba5a50e57ede27bbb43eeb4c2a0723b63a5e6e
sanjeetboora <sanjeet.boora97@gmail.com>
2020-02-01 07:58:07 +0530

    codes added

--- CodingBlocks/Pointer Approaches/targetSumPair.cpp
+++ CodingBlocks/Pointer Approaches/targetSumPair.cpp
@@ -1,34 +1,38 @@
+//https://hack.codingblocks.com/app/practice/1/p/211
 #include<bits/stdc++.h>
 using namespace std;
 
 void targetSumPairs(int *arr, int size, int target) {
-	int pairs = 0;
-	int tempArr[10000] = {0};
-	for (int i = 0; i < size; ++i)
-	{
-		tempArr[arr[i]]++;
-		cout << tempArr[arr[i]] << endl;
-	}
 
-	for (int i = 0; i < size ; ++i)
+	sort(arr, arr + size);
+	int i = 0, j = size - 1;
+	while (i < j)
 	{
-		if (tempArr[target - arr[i]] > 0) {
-			pairs += 1;
+		if (arr[i] + arr[j] == target) {
+			cout << arr[i] << " and " << arr[j] << endl;
+			i++;
+			j--;
 		}
-
+		else if (arr[i] + arr[j] > target) {
+			j--;
+		}
+		else {
+			i++;
+		}
 	}
-	cout << pairs / 2 << endl;
+
 }
 
 int main(int argc, char const *argv[])
 {
 	int size, target;
-	cin >> size >> target;
+	cin >> size;
 	int* arr = new int[size];
 	for (int i = 0; i < size; ++i)
 	{
 		cin >> arr[i];
 	}
+	cin >> target;
 	targetSumPairs(arr, size, target);
 
 	return 0;
