c404c2c85b21574193aee2a26cfa084dcb4d1c07
sanjeetboora <sanjeet.boora97@gmail.com>
2020-02-04 13:27:31 +0530

    codes added

--- CodingBlocks/Recursion/subsequence.cpp
+++ CodingBlocks/Recursion/subsequence.cpp
@@ -1,25 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
-void subsequence(string input,string output){
+void subsequence(string input, string output) {
 
-	if(input.length() == 0){
-		cout<<output<<endl;
+	if (input.length() == 0) {
+		cout << output << endl;
 		return;
 	}
 
 	char ch = input[0];
 	string ros = input.substr(1);
-	subsequence(ros, output+ch);
+	subsequence(ros, output + ch);
 	subsequence(ros, output);
 
 }
 
 int main()
-{	
+{
 	string str;
-	cin>>str;
+	cin >> str;
 
-	subsequence(str,"");
+	subsequence(str, "");
 
 	return 0;
 }
