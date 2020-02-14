ed32627462f7015f70c3ac839be74965fe0d5c8c
sanjeetboora <sanjeet.boora97@gmail.com>
2020-02-14 15:32:36 +0530

    codes added

--- Leetcode/Arrays/trappingRainWaterStack.cpp
+++ Leetcode/Arrays/trappingRainWaterStack.cpp
@@ -1,3 +1,28 @@
 //https://leetcode.com/problems/trapping-rain-water
 
 
+class Solution {
+public:
+	int trap(vector<int>& height) {
+		int area = 0;
+
+		stack<int> st;
+
+		for (int i = 0; i < height.size(); i++) {
+
+			while (!st.empty() && height[i] > height[st.top()] ) {
+				int t = st.top();
+				st.pop();
+				if (st.empty()) {
+					break;
+				}
+				area += (i - st.top() - 1) * (min(height[st.top()], height[i]) - height[t]);
+			}
+
+			st.push(i);
+		}
+
+		return area;
+
+	}
+};
---
+++ Leetcode/Arrays/trappingRainWaterTwoPointer.cpp
@@ -0,0 +1,35 @@
+//https://leetcode.com/problems/trapping-rain-water
+
+class Solution {
+public:
+	int trap(vector<int>& height) {
+		int area = 0;
+
+		int left = 0, leftMax = 0, right = height.size() - 1, rightMax = 0;
+
+		while (left < right) {
+			if (height[left] < height[right]) {
+				if (height[left] > leftMax) {
+					leftMax = height[left];
+				}
+				else {
+					area += (leftMax - height[left]);
+				}
+				left++;
+			}
+			else {
+				if (height[right] > rightMax) {
+					rightMax = height[right];
+				}
+				else {
+					area += (rightMax - height[right]);
+				}
+				right--;
+			}
+
+		}
+
+		return area;
+
+	}
+};
