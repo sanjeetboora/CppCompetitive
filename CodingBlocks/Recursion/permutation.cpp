16c2a2d91a496081e850bbc6e1751e324ee737b7
sanjeetboora <sanjeet.boora97@gmail.com>
2020-02-16 09:40:23 +0530

    codes added

---
+++ CodingBlocks/Recursion/permutation.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printPermutations(string input, string output){
+	if(input.length() == 0){
+		cout<<output<<endl;
+		return;
+	}
+
+	for (int i = 0; i < input.length(); ++i)
+	{
+		char ch = input[i];
+		string ros = input.substr(0,i) + input.substr(i+1);
+		printPermutations(ros, output+ch);
+	}
+	
+	return;
+}
+
+int main(int argc, char const *argv[])
+{
+	string str;
+	cin>>str;
+	printPermutations(str,"");
+
+	
+
+	return 0;
+}
--- Leetcode/Arrays/trappingRainWaterTwoPointer.cpp
+++ Leetcode/Arrays/trappingRainWaterTwoPointer.cpp
@@ -4,9 +4,7 @@ class Solution {
 public:
 	int trap(vector<int>& height) {
 		int area = 0;
-
 		int left = 0, leftMax = 0, right = height.size() - 1, rightMax = 0;
-
 		while (left < right) {
 			if (height[left] < height[right]) {
 				if (height[left] > leftMax) {
