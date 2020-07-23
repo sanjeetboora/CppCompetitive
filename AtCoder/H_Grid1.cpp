93706a141931f3e4c29d5b92086a28c075905518
sanjeetboora <sanjeet.boora97@gmail.com>
2020-07-24 05:09:02 +0530

    H-Grid 1 added

---
+++ AtCoder/H_Grid1.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+#define mod 1000000007
+
+int totalPaths(int grid[1005][1005], int h, int w) {
+
+	int dp[1005][1005] = {0};
+	dp[h - 1][w - 1] = 0;
+	dp[h - 1][w - 2] = 1;
+	dp[h - 2][w - 1] = 1;
+	for (int i = h - 1; i >= 0; i--) {
+		for (int j = w - 1; j >= 0; j--) {
+			if (grid[i][j] == 0) {
+				if (i + 1 <= h - 1) {
+					dp[i][j] += dp[i + 1][j];
+				}
+				if (j + 1 <= w - 1) {
+					dp[i][j] = ((dp[i][j] % mod) + (dp[i][j + 1] % mod)) % mod;
+				}
+			} else {
+				dp[i][j] = 0;
+			}
+		}
+	}
+
+	return  dp[0][0];
+}
+
+
+int main() {
+	int h, w;
+	cin >> h >> w;
+	int grid[1005][1005];
+	for (int i = 0; i < h; i++) {
+		for (int j = 0; j < w; j++) {
+			char x;
+			cin >> x;
+			x == '.' ? grid[i][j] = 0 : grid[i][j] = 1;
+		}
+	}
+	cout << totalPaths(grid, h, w) << endl;
+}
