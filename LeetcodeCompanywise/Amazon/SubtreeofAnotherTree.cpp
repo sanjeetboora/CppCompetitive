789d33f9efaa95a2b30892eeb358f32b9d84573a
sanjeetboora <sanjeet.boora97@gmail.com>
2020-09-13 15:42:53 +0530

    codes added

--- LeetcodeCompanywise/Amazon/SubtreeofAnotherTree.cpp
+++ LeetcodeCompanywise/Amazon/SubtreeofAnotherTree.cpp
@@ -1,3 +1,6 @@
+https://leetcode.com/problems/subtree-of-another-tree/
+// O(m^2+n^2+m*n)
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -35,6 +38,40 @@ public:
         }
         return false;
 
+
+    }
+};
+
+
+//--------- Another Optimised Version -----------
+// O(m*n)
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    bool equal(TreeNode* x, TreeNode* y) {
+        if (x == NULL and y == NULL) return true;
+        if (x == NULL or y == NULL) return false;
+
+        return (x->val == y->val) and equal (x->left, y->left) and equal(x->right, y->right);
+
+
+    }
+
+    bool isSubtree(TreeNode* s, TreeNode* t) {
+
+        return s != NULL and (equal(s, t) or isSubtree(s-> left, t) or isSubtree(s->right, t));
 
     }
 };
