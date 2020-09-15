8632c541c7609bf21460406eb49da41e17ec2556
sanjeetboora <sanjeet.boora97@gmail.com>
2020-09-16 00:11:02 +0530

    codes added

---
+++ LeetcodeCompanywise/Amazon/Week1/TwoSum.cpp
@@ -0,0 +1,19 @@
+//https://leetcode.com/problems/two-sum/
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> freqMap;
+        vector<int> result;
+        for (int i = 0; i < nums.size(); i++) {
+            freqMap[nums[i]] = i;
+        }
+
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (freqMap.count(target - nums[i]) and freqMap[target - nums[i]] != i) {
+                return {i, freqMap[target - nums[i]]};
+            }
+        }
+        return result;
+    }
+};
