7f6f9a25caf462a77894bf60c1b9a35907befeaf
sanjeetboora <sanjeet.boora97@gmail.com>
2020-09-16 12:05:36 +0530

    codes added

--- LeetcodeCompanywise/Amazon/Week1/LRUCache.cpp
+++ LeetcodeCompanywise/Amazon/Week1/LRUCache.cpp
@@ -1,3 +1,7 @@
+/* https://leetcode.com/problems/lru-cache/ */
+
+/* Using hashmap and doubly linked list */
+
 class Node {
 public :
     int keyData;
@@ -82,6 +86,94 @@ public:
                 llSize++;
             }
 
+        }
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
+
+/* ---------------------------------------------------- */
+
+class Node {
+public :
+    int keyData;
+    int valData;
+    Node* prev;
+    Node* next;
+};
+class LRUCache {
+public:
+    unordered_map<int, Node*> mp;
+    Node* head;
+    Node* tail;
+    int maxSize;
+    LRUCache(int capacity) {
+        maxSize = capacity;
+        head = new Node();
+        tail = new Node();
+
+        head -> next = tail;
+        head -> prev = NULL;
+        tail -> prev = head;
+        tail -> next = NULL;
+    }
+
+    void removeNode(int k) {
+        Node* n = mp[k];
+        n->next -> prev = n->prev;
+        n->prev->next = n->next;
+        mp.erase(k);
+        delete n;
+    }
+
+    void removeEnd() {
+        Node* n = tail->prev;
+
+        n->next->prev = n->prev;
+        n->prev->next = n->next;
+        mp.erase(n->keyData);
+        delete n;
+    }
+
+    void addFront(int k, int val) {
+        Node* n = new Node();
+        n -> keyData = k;
+        n -> valData = val;
+        n->next = head->next;
+        n->prev = head;
+        head -> next -> prev = n;
+        head -> next = n;
+
+        mp[k] = n;
+    }
+
+
+    int get(int key) {
+        if (mp.count(key)) {
+            int val = mp[key]->valData;
+            removeNode(key);
+            addFront(key, val);
+            return mp[key] -> valData;
+        }
+
+        return -1;
+    }
+
+    void put(int key, int value) {
+        if (mp.count(key)) {
+            removeNode(key);
+            addFront(key, value);
+        }
+        else {
+            if (mp.size() >= maxSize) {
+                removeEnd();
+            }
+            addFront(key, value);
         }
     }
 };
