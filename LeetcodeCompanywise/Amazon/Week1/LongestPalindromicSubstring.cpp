832d1ae781ef15788ab453785e1cf8beab095c50
sanjeetboora <sanjeet.boora97@gmail.com>
2020-09-17 07:33:07 +0530

    codes added

--- LeetcodeCompanywise/Amazon/Week1/LongestPalindromicSubstring.cpp
+++ LeetcodeCompanywise/Amazon/Week1/LongestPalindromicSubstring.cpp
@@ -1,62 +1,161 @@
 /* https://leetcode.com/problems/longest-palindromic-substring/ */
 
+/* Dynamic Programming  O(N^2)*/
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        string res = "";
+        int sz = s.length();
+        if (!sz) return res;
+
+        int **arr = new int*[sz];
+
+        for (int i = 0; i < sz; i++) {
+            arr[i] = new int[sz];
+        }
+
+        pair<int, int> p = {0, 0};
+
+        for (int ws = 0; ws <= 1; ws++) {
+            for (int i = 0; i < sz; i++) {
+                for (int j = i; j <= i + ws and j < sz; j++) {
+                    if (s[i] == s[j]) {
+                        arr[i][j] = 1;
+
+                        if (j - i > p.second - p.first) {
+                            p = {i, j};
+                        }
+                    }
+                    else {
+                        arr[i][j] = 0;
+                    }
+                }
+            }
+        }
+
+        for (int ws = 2; ws < sz; ws++) {
+            for (int i = 0; i < sz - 2; i++) {
+                for (int j = i + ws; j < sz; j++) {
+                    if (s[i] == s[j] and arr[i + 1][j - 1] == 1) {
+                        arr[i][j] = 1;
+
+                        if (j - i > p.second - p.first) {
+                            p = {i, j};
+                        }
+                    }
+                    else {
+                        arr[i][j] = 0;
+                    }
+                }
+            }
+        }
+        res = s.substr(p.first, p.second - p.first + 1);
+        return res;
+    }
+};
+
+/* Expand Around Center  O(N^2)*/
+class Solution {
+public:
+    pair<int, int> expandAround(string str, int l, int r) {
+        pair<int, int>p;
+        while (l >= 0 and r < str.length() and str[l] == str[r]) {
+            p = {l, r};
+            l--;
+            r++;
+        }
+        return p;
+    }
+    string longestPalindrome(string s) {
+        string res = "";
+        int sz = s.length();
+        if (!sz) return res;
+        int l = 0, maxlen = 0;
+        for (int i = 0; i < sz; i++) {
+            pair<int, int>p1 = expandAround(s, i, i);
+            pair<int, int>p2 = expandAround(s, i, i + 1);
+
+            if ((p1.second - p1.first + 1 ) > maxlen) {
+                l = p1.first;
+                maxlen = p1.second - p1.first + 1;
+            }
+
+            if ((p2.second - p2.first + 1 ) > maxlen) {
+                l = p2.first;
+                maxlen = p2.second - p2.first + 1;
+            }
+        }
+        res = s.substr(l, maxlen);
+        return res;
+    }
+};
+
+
 
 /*Manacher's Algo  O(N)*/
 
 class Solution {
 public:
     string addBoundaries(string str) {
-        string res = "@#";
-
+        string modified = "@#";
         for (char s : str) {
-            res += s;
-            res += "#";
+            modified += s;
+            modified += "#";
         }
-        res += "$";
-        return res;
+        modified += "$";
+        return modified;
     }
-    string palString(string modified, int maxidx, int val) {
-        string res = modified.substr(maxidx - val, (val * 2) + 1);
-        for (int i = 0; i < res.length(); i++) {
-            if (res[i] != '#') {
-                result += res[i];
-            }
-        }
-        return result;
 
-
-    }
     string longestPalindrome(string s) {
         string res = "";
-        if (s.length() == 0) return res;
+
+        int sz = s.length();
+        if (sz == 0) return res;
+
         string modified = addBoundaries(s);
         int* arr = new int[modified.length()];
         for (int i = 0; i < modified.length(); i++) arr[i] = 0;
-        int c = 1, r = 1;
-        for (int i = 1; i < modified.length() - 1; i++) {
-            int mirr  = 2 * c - i;
 
+        int c = 1, r = 1, maxlen = 0, maxidx = 0;
+        for (int i = 1; i < modified.length() - 1; i++) {
+            int mirr = 2 * c - i;
             if (i < r) {
                 arr[i] = min(arr[mirr], r - i);
             }
 
             while (modified[i + (1 + arr[i])] == modified[i - (1 + arr[i])]) {
                 arr[i]++;
-
             }
 
             if (i + arr[i] > r) {
                 r = i + arr[i];
                 c = i;
+                if (maxlen < arr[i]) {
+                    maxlen = arr[i];
+                    maxidx = i;
+                }
             }
         }
-        cout << endl;
 
-        int maxidx = max_element(arr, arr + modified.length()) -  arr;
-
-        res = palString(modified, maxidx, arr[maxidx]);
+        for (int i = maxidx - maxlen; i <= maxidx + maxlen; i++) {
+            if (modified[i] != '#') {
+                res += modified[i];
+            }
+        }
 
         return res;
 
+
     }
 };
+
+
+
+
+
+
+
+
+
+
+
