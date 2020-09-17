8023db515551084f2a6a336022d994967479e04d
sanjeetboora <sanjeet.boora97@gmail.com>
2020-09-17 15:08:32 +0530

    codes added

---
+++ LeetcodeCompanywise/Amazon/Week1/KClosestPointsToOrigin.cpp
@@ -0,0 +1,30 @@
+/*  https://leetcode.com/problems/k-closest-points-to-origin/  */
+
+class Solution {
+public:
+
+    int dist(vector<int> x) {
+        return x[0] * x[0] + x[1] * x[1];
+    }
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
+        priority_queue<pair<int, vector<int> > >pq;
+        vector<vector<int> >res;
+        int i = 0;
+        while (i < points.size()) {
+
+            if (pq.size() < K) {
+                pq.push({dist(points[i]), points[i]});
+            }
+            else if (pq.top().first > dist(points[i])) {
+                pq.pop();
+                pq.push({dist(points[i]), points[i]});
+            }
+            i++;
+        }
+        while (!pq.empty()) {
+            res.push_back(pq.top().second);
+            pq.pop();
+        }
+        return res;
+    }
+};
