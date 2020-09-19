a5a561facf047611f475b2b4d4029adb0b084b22
sanjeetboora <sanjeet.boora97@gmail.com>
2020-09-19 11:33:56 +0530

    codes added

--- LeetcodeCompanywise/Amazon/Week1/WordLadder.cpp
+++ LeetcodeCompanywise/Amazon/Week1/WordLadder.cpp
@@ -1,5 +1,6 @@
 //https://leetcode.com/problems/word-ladder/
 
+//using bi-directional  | Time: N*(26^(M/2))  | N - number of words |  M - length of a word
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
@@ -40,6 +41,45 @@ public:
             ladder++;
         }
 
+        return 0;
+    }
+};
+
+
+
+//using bfs Time: N*(26^M)  | N - number of words |  M - length of a word
+
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string> dict(wordList.begin(), wordList.end());
+        if (dict.find(endWord) == dict.end()) return 0;
+        queue<string> todo;
+        todo.push(beginWord);
+        int ladder = 1;
+        while (!todo.empty()) {
+            int n = todo.size();
+            for (int i = 0; i < n; i++) {
+                string word = todo.front();
+                todo.pop();
+                if (word == endWord) {
+                    return ladder;
+                }
+                dict.erase(word);
+                for (int j = 0; j < word.size(); j++) {
+                    char c = word[j];
+                    for (int k = 0; k < 26; k++) {
+                        word[j] = 'a' + k;
+                        if (dict.find(word) != dict.end()) {
+                            todo.push(word);
+                            dict.erase(word);
+                        }
+                    }
+                    word[j] = c;
+                }
+            }
+            ladder++;
+        }
         return 0;
     }
 };
